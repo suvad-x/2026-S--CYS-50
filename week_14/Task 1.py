8476f15a7e6b3a3bcb73adbfbe5e2155bf2eb048
Dawar <155221945+suvad-x@users.noreply.github.com>
2026-07-01 00:59:26 +0500

    Add files via upload

---
+++ week_14/Task 1.py
@@ -0,0 +1,7 @@
+def show_intro():
+    print("Welcome to class!!")
+def show_intro_two():
+    print("Welcome to class2!!")
+if __name__ == "__main__":
+    show_intro()
+    show_intro_two()
---
+++ week_14/Task 2.py
@@ -0,0 +1,8 @@
+def display_message():
+    print("Welcome to class!!")
+
+def display_message_two():
+    print("Welcome to class2!!")
+
+if __name__ == "__main__":
+    display_message()
